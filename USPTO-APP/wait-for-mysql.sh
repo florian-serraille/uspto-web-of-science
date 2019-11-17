287bdda9fcea0ffdba574c9bcd65fd9fca543463
Florian Serraille <florianserraille@gmail.com>
2019-11-17 13:26:54 -0300

    Tirar as mensagens de erro do banco de dado

--- USPTO-APP/wait-for-mysql.sh
+++ USPTO-APP/wait-for-mysql.sh
@@ -6,7 +6,7 @@ DATABASE=USPTO
 USER=USPTO-APP
 PWD=UCB
 
-until mysql -h${HOST} -u${USER} -p${PWD} ${DATABASE} -e '\q' > /dev/null 2>&1 ; do
+until mysql -h${HOST} -u${USER} -p${PWD} ${DATABASE} -e '\q' 2> /dev/null; do
   echo "MySQL is unavailable - sleeping"
   sleep 3
 done
